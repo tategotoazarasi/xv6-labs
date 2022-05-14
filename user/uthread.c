06125741e8250948deb9a76cd8aecbe4a4ee006b
Tategoto Azarasi <2724167997@qq.com>
2022-05-14 19:48:30 +0800

    Uthread: switching between threads
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,34 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;       /* the thread's context */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+
 void 
 thread_init(void)
 {
@@ -35,7 +54,6 @@ void
 thread_schedule(void)
 {
   struct thread *t, *next_thread;
-
   /* Find another runnable thread. */
   next_thread = 0;
   t = current_thread + 1;
@@ -62,8 +80,10 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-  } else
+    thread_switch((uint64)(&t->context), (uint64)(&next_thread->context));
+  } else {
     next_thread = 0;
+  }
 }
 
 void 
@@ -76,6 +96,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)(func);
+  t->context.sp = (uint64)(t->stack+STACK_SIZE);
 }
 
 void 
